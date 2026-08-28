440bfc2e04d094dbb7791b3249985854ba55ee77
Wayne <wanjiahao389@gmail.com>
2026-08-28 14:15:37 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase 配置 - 从 Firebase Console 获取
+// https://console.firebase.google.com → 项目设置 → 常规 → 你的应用
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCGLQ0wzn3355Fi2RQEF3AyXple0Lr5y8",
+    authDomain: "wayne-chat.firebaseapp.com",
+    databaseURL: "https://wayne-chat-default-rtdb.firebaseio.com",
+    projectId: "wayne-chat",
+    storageBucket: "wayne-chat.appspot.com",
+    messagingSenderId: "18461747473",
+    appId: "1:18461747473:web:e5bec51f24e7608ebf04b0",
+    measurementId: "G-75HB9FQ9YJ"
+};
